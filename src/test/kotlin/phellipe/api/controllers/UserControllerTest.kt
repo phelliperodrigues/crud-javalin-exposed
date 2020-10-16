1d7302d6d68c3c0330bb6738d62e7edfbcc91747
Phellipe Rodrigues <phellipe-rodrigues@phellipe.rodrigues>
2020-10-16 18:27:50 -0300

    Start UserController

---
+++ src/test/kotlin/phellipe/api/controllers/UserControllerTest.kt
@@ -0,0 +1,12 @@
+import io.javalin.Javalin
+import org.junit.jupiter.api.BeforeEach
+import phellipe.utils.HttpUtil
+
+class UserControllerTest {
+    private lateinit var app: Javalin
+    private lateinit var http: HttpUtil
+
+    @BeforeEach
+    fun start() {
+    }
+}
